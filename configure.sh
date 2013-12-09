811bafbc6fbb7ee2b69204864d0028c292b66449
Limbeck Engineering LLC <limbeck.engineering@gmail.com>
2013-12-09 13:28:14 -0500

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -7,3 +7,20 @@ if (( $EUID != 0 )); then
    exit 1 
 fi
 
+echo "$(tput setaf 6) This scripts will configure your Raspberry Pi for _________________________.$(tput sgr0)"
+
+echo "$(tput setaf 6)Software currently compatible with RASPBIAN only.$(tput sgr0)"
+
+read -p "$(tput bold ; tput setaf 2)Press [Enter] to begin, [Ctrl-C] to abort...$(tput sgr0)"
+
+echo "$(tput setaf 6)Updating Raspbian Packages....$(tput sgr0)"
+apt-get update -q -y
+
+echo "$(tput setaf 6)Installing Java 7...$(tput sgr0)"
+apt-get install openjdk-7-jre
+apt-get install openjdk-7-jdk
+
+echo "$(tput setaf 6)Installing the latest RXTX Libraries...$(tput sgr0)"
+apt-get install librxtx-java
+
+
